f2bfc0cf52f6dd526bb9561f704d1dd5c9a48383
Jake Gealer <jake@gealer.email>
2019-08-11 13:36:14 +0100

    Vlad said this should work better

--- rethink_connection.js
+++ rethink_connection.js
@@ -1,10 +1,7 @@
 module.exports = require("rethinkdbdash")({
-    servers: [
-        {
-            host: process.env.RETHINKDB_HOSTNAME ? process.env.RETHINKDB_HOSTNAME : "127.0.0.1",
-            port: 28015,
-        },
-    ],
+    pool: true,
+    host: process.env.RETHINKDB_HOSTNAME || '127.0.0.1',
+    port: 28015,
     db: "magiccap",
     password: process.env.RETHINKDB_PASSWORD,
     user: process.env.RETHINKDB_USER,
